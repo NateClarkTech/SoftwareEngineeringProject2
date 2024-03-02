febc2c212d5895e472d0c994340e841f62662acf
NateClarkTech <NateClarkTech@gmail.com>
2024-03-02 00:19:32 -0500

    added modals

--- NovoTaskNinja/models.py
+++ NovoTaskNinja/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django import forms
 from .models import *
+from django.contrib.auth.models import User
 
 class Meta:
     ordering = ['name']
@@ -8,6 +9,7 @@ class Meta:
 class Task(models.Model):
 
     name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, default="")
     id = models.AutoField(primary_key=True)
     completed = models.BooleanField(default=False)
     # everytime an item is created, it will be automatically added
@@ -15,17 +17,14 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
-class TodoItem(models.Model):
-    description = models.CharField(max_length=255)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.description
+class ShoppingList(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    boxNumber = models.IntegerField()
+    checked = models.BooleanField(default=False)
 
 class InProgressCourse(models.Model):
-    name = models.CharField(max_length=100)
-    date_added = models.DateTimeField(auto_now_add=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
     start_time = models.TimeField(null=True, blank=True)
     end_time = models.TimeField(null=True, blank=True)
     days_of_week = models.CharField(max_length=5, null=True, blank=True)  # e.g. 'MWF' for Monday, Wednesday, Friday
@@ -34,14 +33,14 @@ class InProgressCourse(models.Model):
         return f"{self.name} ({self.days_of_week} {self.start_time}-{self.end_time})"
 
 class CompletedCourse(models.Model):
-
-    name = models.CharField(max_length=100)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
     date_completed = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 class CYCRequirement(models.Model):
+    completedByCourse = models.ForeignKey(CompletedCourse, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     is_completed = models.BooleanField(default=False)
 
@@ -56,3 +55,27 @@ class foodDay(models.Model):
     def __str__(self):
         return self.name
 
+
+class Event:
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    startTime = models.DateTimeField(auto_now_add=True)
+    endTime = models.DateTimeField(auto_now_add=True)
+    eventType = models.CharField(max_length=200)
+    frequency = models.CharField(max_length=200)
+
+class Chatroom(models.Model):
+    roomName = models.CharField(max_length=200)
+
+class Message(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    message = models.CharField(max_length=2000)
+    #days_of_week = models.CharField(max_length=7, null=True, blank=True)  # e.g. 'MWF' for Monday, Wednesday, Friday
+
+    def __str__(self):
+        return self.name
+    
+class Leaderboard():
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
