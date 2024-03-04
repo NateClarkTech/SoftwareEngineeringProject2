6ca80cef02a0c15a7e2c04ce4bc7f1bd533496f9
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2024-03-03 20:26:16 -0500

    Added a source for my fix, and moved leaderboards/ up on the urlpatterns

--- Leaderboard/urls.py
+++ Leaderboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+#https://www.reddit.com/r/django/comments/13hknj7/my_project_doesnt_recognize_the_urls_from_one_of/
 urlpatterns = [
+    path('', views.checklist, name='checklist'),
     path('checklist/', views.checklist, name='checklist'),
 ]
--- SEProject2/urls.py
+++ SEProject2/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('leaderboard/', include('Leaderboard.urls')),
     path('admin/', admin.site.urls),
     path('', include('NovoTaskNinja.urls')),
-    path('leaderboard/', include('Leaderboard.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
