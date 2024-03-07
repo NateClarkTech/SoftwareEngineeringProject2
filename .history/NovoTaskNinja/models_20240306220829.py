973c72a2f3f84076b40330d84bd4da0ffa86044f
NateClarkTech <NateClarkTech@gmail.com>
2024-03-06 22:10:38 -0500

    Added Shopping List App

--- .history/NovoTaskNinja/models_20240306220829.py
+++ .history/NovoTaskNinja/models_20240306220829.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django import forms
 from .models import *
-from django.contrib.auth.models import User
+from authentication.models import User
 
 class Meta:
     ordering = ['name']
