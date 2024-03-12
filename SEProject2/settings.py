af57253813b3920c83ba7284630b33aece0b077b
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2024-03-12 19:52:35 -0400

    Updated chat, and installed required software

--- SEProject2/settings.py
+++ SEProject2/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'NovoTaskNinja',
     'Leaderboard',
     'Profile',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
