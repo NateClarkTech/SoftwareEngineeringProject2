25dc074d1d5e7780b4169096d242f98aad60da5a
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2024-03-13 15:44:15 -0400

    add default pfp, and make home button take the user to the home page.

--- Profile/models.py
+++ Profile/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pics', default='default.jpg')
+    profile_picture = models.ImageField(upload_to='profile_pics', default='profile_pics/default.png')
     personal_info = models.TextField(max_length=500, blank=True)
     
     
