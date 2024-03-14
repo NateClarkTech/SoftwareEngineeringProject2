63a9520e6c433c1cabc7365e6edf6bbecc56b9c4
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2024-03-13 20:25:31 -0400

    Fixed broken links on the home page

--- ShoppingList/urls.py
+++ ShoppingList/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
+
+
 urlpatterns = [
     path('', views.shoppinglist, name='shoppinglist'),
     path('update_shoppinglist', views.update_shoppinglist, name='update_shoppinglist'),
