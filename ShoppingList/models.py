67b43ef7fdc03eb48113a770c4bbf208f87b9556
Nate Clark <129341001+NateClarkTech@users.noreply.github.com>
2024-03-17 13:29:31 -0400

    Update models.py

--- ShoppingList/models.py
+++ ShoppingList/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+#Shopping list category that shopping list items point to
 class ShoppingListCategory(models.Model):
+    #Name of the category
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+#Shopping list item modal
+#points to a user, stores the name of the shopping list item
+#points to the category it is apart of
+#stores a link to an external page (store)
+#Stores if the item is checked off or not
+#NOTE: Extendos the todo item from project 1 but adds the feilds a shopping list item would need
 class ShoppingListItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     name = models.CharField(max_length=200)
